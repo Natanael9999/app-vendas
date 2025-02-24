4dfaa14a3919afb5c4e2bbcaa32e68369577f83e
Natanael9999 <samn14032622@gmail.com>
2025-02-24 14:29:45 -0300

    first commit

---
+++ src/app/api-vercel/produtos/page.jsx
@@ -0,0 +1,23 @@
+export default async function Produtos(){
+    
+    const recebedados = await fetch('https://api.vercel.app/products');
+    const produtos = await recebedados.json();
+
+    return(
+        <>
+
+            <ul>
+                {
+                    produtos.map ((produto) => (
+                        <li key={produto.key}>
+                            {produto.id}
+                            {produto.name}
+                        </li>
+                    ))
+                }
+            </ul>
+
+        </>
+    )
+
+}
